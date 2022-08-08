52661626904e800f79050055b272de765f2babbf
D11Jose <j.iznardo@double11.com>
2022-08-08 20:07:09 +0100

    quick platform detection for GLSL version

--- game/src/core/platform_detection.hpp
+++ game/src/core/platform_detection.hpp
@@ -39,3 +39,9 @@
 	/* Unknown compiler/platform */
 #error "Unknown platform!"
 #endif // End of platform detection
+
+#if defined(PLATFORM_DESKTOP)
+#define GLSL_VERSION            330
+#else   // PLATFORM_RPI, PLATFORM_ANDROID, PLATFORM_WEB
+#define GLSL_VERSION            100
+#endif
