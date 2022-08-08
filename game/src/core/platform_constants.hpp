bcf4e5bd0a89aecb9dc0b6e4adb1c1d0e0036f40
D11Jose <j.iznardo@double11.com>
2022-08-08 20:22:37 +0100

    add platform constants

---
+++ game/src/core/platform_constants.hpp
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include "core/platform_detection.hpp"
+
+#if defined(PLATFORM_DESKTOP)
+    #define GLSL_VERSION            330
+    #else   // PLATFORM_RPI, PLATFORM_ANDROID, PLATFORM_WEB
+    #define GLSL_VERSION            100
+#endif
+
+// NOTE: Gamepad name ID depends on drivers and OS
+#define XBOX360_LEGACY_NAME_ID  "Xbox Controller"
+#if defined(PLATFORM_RPI)
+    #define XBOX360_NAME_ID     "Microsoft X-Box 360 pad"
+    #define PS3_NAME_ID         "PLAYSTATION(R)3 Controller"
+#else
+    #define XBOX360_NAME_ID     "Xbox 360 Controller"
+    #define PS3_NAME_ID         "PLAYSTATION(R)3 Controller"
+#endif
